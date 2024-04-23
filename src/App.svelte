66cd5d16e6d9c9ffeec5feaefd009d4276840be8
Mark Teofel S. Cernal <20103214@usc.edu.ph>
2024-04-23 22:05:19 +0800

    Commit the files

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+  let people = [
+    {
+      fullname: 'Sophia Lorraine Tecson',
+      age: 22,
+      status: 'single',
+      id:0
+    },
+    {
+      fullname: 'Mark Teofel Cernal ',
+      age: 23,
+      status: 'single',
+      id:1
+    },
+    {
+      fullname: 'Kabit ni Sophia',
+      age: 22,
+      status: 'single',
+      id:2
+    }
+  ]
+
+  const handleClick = (id) =>{ 
+    people = people.filter((person) => person.id != id)
+  }
+
+</script>
+
+<main>
+  {#each people as person (person.id)}
+    <div>
+      <h3>{person.fullname}</h3>
+      <p>{person.age} years old</p>
+      <p>{person.status}</p>
+      <button on:click={() => handleClick(person.id)}>Delete</button>
+    </div>
+  {:else}
+    <p>No person here</p>
+  {/each}
+  <div>
+  </div>
+</main>
+
+<style>
+
+</style>
